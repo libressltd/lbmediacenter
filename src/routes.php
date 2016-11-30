3490d8be1e4464c5e3b0c1047eaf8cea579b16bb
iConcept <iconcept@MacBook-Pro.local>
2016-11-30 18:37:55 +0700

    Test with multiple resource

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,10 @@
 <?php
 
-Route::group(['middleware' => 'web'], function () {
+Route::group(['middleware' => ['web', 'auth']], function () {
 	Route::resource("lbmedia", "libressltd\lbmediacenter\controllers\MediaController");
+});
+
+
+Route::group(['middleware' => ['api', 'auth:api']], function () {
+	Route::resource("api/lbmedia", "libressltd\lbmediacenter\controllers\MediaController");
 });
